6a8ae8514cf18ff10394026b242b33818f07b252
rhyzeline-macapagal <rhyzelinemacapagal@gmail.com>
2025-09-29 20:07:11 +0800

    salary pond

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
     implementation ("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
-    implementation("com.github.mhiew:android-pdf-viewer:3.2.0-beta.1")
+
 
 
 
